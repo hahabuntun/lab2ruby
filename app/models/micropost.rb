0bbabe93a516466dc3e1ed4d3e78d036e66571e6
hahabuntun <hahabuntun@gmail.com>
2024-09-26 18:54:39 +0300

    finished lab2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ApplicationRecord
+    belongs_to :user
+    validates :content, presence: true, length: { maximum: 140 }
+
+    def check_user_id_exists
+        errors.add(:user_id, "User must exist") unless User.exists?(user_id)
+    end
 end
+  
